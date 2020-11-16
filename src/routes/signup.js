216c6f68f7f5138e1aed978ca38fb9c5f068a384
Mathieu Boisvert <boisvert.my@gmail.com>
2020-11-16 11:33:00 -0500

    Ajout de validation 'dynamique' dans le form d'inscription. Quand tout est beau aucunes erreurs s'affiche. Reste à ajouter de la validation sur le password

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -5,10 +5,14 @@ const Utilisateur = require('../models/utilisateurSchema');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('signup');
+    res.render('signup', {
+        data: {},
+        errors: {}
+    });
 });
 
 /* Handler POST */
+/*
 router.post('/', function (req, res, next) {
 	let username = req.body.username.trim();
 	let email = req.body.email.trim();
@@ -24,7 +28,31 @@ router.post('/', function (req, res, next) {
 		'confPwd' : confPwd
 	};
 
-	res.send(user);
+	res.render('signup', {getInfo: req.body});
+});
+ */
+
+const {check, validationResult, matchedData} = require('express-validator');
+router.post('/', [
+    check('username')
+        .not().isEmpty()
+        .withMessage('Username is required')
+        .trim(),
+    check('email')
+        .isEmail()
+        .withMessage('Email must be an email and is required')
+        .bail()
+        .trim()
+        .normalizeEmail()
+], (req, res) => {
+    const errors = validationResult(req);
+    res.render('signup', {
+        data: req.body,
+        errors: errors.mapped()
+    });
+
+    const data = matchedData(req);
+    console.log('Sanitized:', data);
 });
 
 module.exports = router;
